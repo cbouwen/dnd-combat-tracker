e6470ca970bc7ccb7d125f6255447d2d5eca52ac
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-05-03 17:26:10 +0200

    Initial commit

---
+++ src/main/java/com/example/dnd_combat_tracker/DndCombatTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dnd_combat_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndCombatTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndCombatTrackerApplication.class, args);
+	}
+
+}
