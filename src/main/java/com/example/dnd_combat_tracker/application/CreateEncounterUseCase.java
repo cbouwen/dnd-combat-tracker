f89ec5c4ae3d5f220f4e0bddbecd3a88de8855ef
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-05-07 14:25:13 +0200

    Encounters are now stored in memory

--- src/main/java/com/example/dnd_combat_tracker/application/CreateEncounterUseCase.java
+++ src/main/java/com/example/dnd_combat_tracker/application/CreateEncounterUseCase.java
@@ -1,5 +1,6 @@
 package com.example.dnd_combat_tracker.application;
 
+import com.example.dnd_combat_tracker.application.ports.EncounterRepositoryPort;
 import com.example.dnd_combat_tracker.domain.CombatEncounter;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,19 @@ import java.util.UUID;
 @Component
 public class CreateEncounterUseCase {
 
+    private final EncounterRepositoryPort encounterRepositoryPort;
+
+    public CreateEncounterUseCase(EncounterRepositoryPort encounterRepositoryPort) {
+        this.encounterRepositoryPort = encounterRepositoryPort;
+    }
+
     public CombatEncounter execute() {
-        return CombatEncounter.create(UUID.randomUUID().toString());
+        if (encounterRepositoryPort.getActive().isPresent()) {
+            throw new IllegalStateException("An active encounter already exists. End it before creating a new one.");
+        }
+        CombatEncounter combatEncounter = CombatEncounter.create(UUID.randomUUID().toString());
+        encounterRepositoryPort.save(combatEncounter);
+        return combatEncounter;
     }
 
 }
---
+++ src/main/java/com/example/dnd_combat_tracker/infrastructure/outbound/EncounterRepositoryAdapter.java
@@ -0,0 +1,27 @@
+package com.example.dnd_combat_tracker.infrastructure.outbound;
+
+import com.example.dnd_combat_tracker.application.ports.EncounterRepositoryPort;
+import com.example.dnd_combat_tracker.domain.CombatEncounter;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class EncounterRepositoryAdapter implements EncounterRepositoryPort {
+    private CombatEncounter activeEncounter;
+
+    @Override
+    public void save(CombatEncounter encounter) {
+            this.activeEncounter = encounter;
+    }
+
+    @Override
+    public Optional<CombatEncounter> getActive() {
+        return Optional.ofNullable(this.activeEncounter);
+    }
+
+    @Override
+    public void clear() {
+        this.activeEncounter = null;
+    }
+}
