9c376fca8e40a844ada45b5715933816a76e7e6d
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-05-12 19:14:50 +0200

    Create use case to start encounter

--- src/main/java/com/example/dnd_combat_tracker/application/StartEncounterUseCase.java
+++ src/main/java/com/example/dnd_combat_tracker/application/StartEncounterUseCase.java
@@ -1,6 +1,8 @@
 package com.example.dnd_combat_tracker.application;
 
+import com.example.dnd_combat_tracker.application.exceptions.CombatantNotFoundException;
 import com.example.dnd_combat_tracker.application.exceptions.EncounterNotFoundException;
+import com.example.dnd_combat_tracker.application.exceptions.NotAllInitiativesSetException;
 import com.example.dnd_combat_tracker.application.ports.EncounterRepositoryPort;
 import com.example.dnd_combat_tracker.domain.CombatEncounter;
 import com.example.dnd_combat_tracker.domain.Combatant;
@@ -22,14 +24,40 @@ public class StartEncounterUseCase {
 
     public void execute(Map<String, Integer> playerInitiatives) {
         CombatEncounter combatEncounter = this.encounterRepositoryPort.getActive().orElseThrow(() -> new EncounterNotFoundException("No active encounter"));
-        playerInitiatives.forEach((combatantId, initiative) -> {
-            combatEncounter.findCombatantById(combatantId)
-                    .ifPresent(combatant -> combatant.setInitiative(initiative));
-        });
-        List<Combatant> npcs = combatEncounter.getCombatants().stream().filter(c -> c.getType() != CombatantType.PC).toList();
-        npcs.forEach(c -> c.setInitiative(rollD20()));
+
+        validatePCsIncluded(playerInitiatives, combatEncounter);
+        validateAndSetPCInitiative(playerInitiatives, combatEncounter);
+        setNPCInitiative(combatEncounter);
+
         combatEncounter.startEncounter();
         this.encounterRepositoryPort.save(combatEncounter);
+    }
+
+    private static void validatePCsIncluded(Map<String, Integer> playerInitiatives, CombatEncounter combatEncounter) {
+        List<Combatant> pcs = combatEncounter.getCombatants().stream()
+                .filter(c -> c.getType() == CombatantType.PC)
+                .toList();
+        pcs.forEach(pc -> {
+            if (!playerInitiatives.containsKey(pc.getId())) {
+                throw new NotAllInitiativesSetException("Missing initiative for PC: " + pc.getName());
+            }
+        });
+    }
+
+    private static void validateAndSetPCInitiative(Map<String, Integer> playerInitiatives, CombatEncounter combatEncounter) {
+        playerInitiatives.forEach((combatantId, initiative) -> {
+            Combatant combatant = combatEncounter.findCombatantById(combatantId)
+                    .orElseThrow(() -> new CombatantNotFoundException("No combatant found for id: " + combatantId));
+            if (initiative == null) {
+                throw new NotAllInitiativesSetException("Initiative cannot be null for combatant: " + combatantId);
+            }
+            combatant.setInitiative(initiative);
+        });
+    }
+
+    private static void setNPCInitiative(CombatEncounter combatEncounter) {
+        List<Combatant> npcs = combatEncounter.getCombatants().stream().filter(c -> c.getType() != CombatantType.PC).toList();
+        npcs.forEach(c -> c.setInitiative(rollD20()));
     }
 
     private static int rollD20() {
