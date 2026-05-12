991ba3ae55e639d8e46cb08ed396057b610b54eb
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-05-12 10:27:57 +0200

    Delete unused custom exception

--- src/main/java/com/example/dnd_combat_tracker/application/exceptions/CombatantNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.example.dnd_combat_tracker.application.exceptions;
-
-public class CombatantNotFound extends RuntimeException {
-    public CombatantNotFound(String message) {
-        super(message);
-    }
-}
