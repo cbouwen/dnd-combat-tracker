96f46f7b5b401add4d63ccd0f22b919f05e01798
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-06-16 14:34:50 +0200

    Delete ActiveEncounterAlreadyExistsException as this does not match with the new business logic anymore

--- src/main/java/com/example/dnd_combat_tracker/application/exceptions/ActiveEncounterAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.dnd_combat_tracker.application.exceptions;
-
-//TODO Delete this and all its usages
-public class ActiveEncounterAlreadyExistsException extends RuntimeException {
-    public ActiveEncounterAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/dnd_combat_tracker/infrastructure/controlleradvice/RestExceptionHandler.java
+++ src/main/java/com/example/dnd_combat_tracker/infrastructure/controlleradvice/RestExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.dnd_combat_tracker.infrastructure.controlleradvice;
 
-import com.example.dnd_combat_tracker.application.exceptions.ActiveEncounterAlreadyExistsException;
 import com.example.dnd_combat_tracker.application.exceptions.EncounterNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,14 +9,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
-
-    @ExceptionHandler(ActiveEncounterAlreadyExistsException.class)
-    protected ResponseEntity<String> handleActiveEncounterExists(ActiveEncounterAlreadyExistsException ex) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .header("Content-Type", MediaType.TEXT_PLAIN_VALUE)
-                .body(ex.getMessage());
-    }
 
     @ExceptionHandler(EncounterNotFoundException.class)
     protected ResponseEntity<String> handleEncounterNotFound(EncounterNotFoundException ex) {
