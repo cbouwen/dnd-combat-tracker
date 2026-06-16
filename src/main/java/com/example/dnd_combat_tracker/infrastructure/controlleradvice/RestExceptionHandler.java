eb3c3a36fd5721f083106a79971bfb54ba69a72b
Cedric Bouwen <cedric.bouwen@hotmail.com>
2026-06-16 17:09:33 +0200

    Return proper HTTP status codes for custom exceptions

--- src/main/java/com/example/dnd_combat_tracker/infrastructure/controlleradvice/RestExceptionHandler.java
+++ src/main/java/com/example/dnd_combat_tracker/infrastructure/controlleradvice/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.dnd_combat_tracker.infrastructure.controlleradvice;
 
+import com.example.dnd_combat_tracker.application.exceptions.CombatantNotFoundException;
 import com.example.dnd_combat_tracker.application.exceptions.EncounterNotFoundException;
+import com.example.dnd_combat_tracker.application.exceptions.NotAllInitiativesSetException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,22 @@ public class RestExceptionHandler {
     protected ResponseEntity<String> handleEncounterNotFound(EncounterNotFoundException ex) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
+                .header("Content-Type", MediaType.TEXT_PLAIN_VALUE)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(CombatantNotFoundException.class)
+    protected ResponseEntity<String> handleCombatantNotFound(CombatantNotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .header("Content-Type", MediaType.TEXT_PLAIN_VALUE)
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(NotAllInitiativesSetException.class)
+    protected ResponseEntity<String> handleNotAllInitiativesSet(NotAllInitiativesSetException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
                 .header("Content-Type", MediaType.TEXT_PLAIN_VALUE)
                 .body(ex.getMessage());
     }
